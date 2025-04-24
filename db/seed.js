d9ba9952191424a9d03ab4b7d2856256c9036cef
Devin Frazier <Dev.fraz18@gmail.com>
2025-04-23 23:49:04 -0400

    Passed all tests

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +9,19 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.between({
+        from: "1991-01-01T00:00:00.000Z",
+        to: "2000-01-01T00:00:00.000Z",
+      }),
+      salary: faker.commerce.price({
+        min: 100000,
+        max: 200000,
+        dec: 0,
+      }),
+    };
+    await createEmployee(employee);
+  }
 }
